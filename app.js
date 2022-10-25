be124330b71c15923f10289ec62bb336ebe418ef
Radu Mircea <mircearadumihai@gmail.com>
2022-10-25 21:19:44 +0300

    delete button/delete-list button

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const addItemInput = document.querySelector("#addItemInput");
 const addItemBtn = document.querySelector("#addItemBtn");
-
+const newListBtn = document.querySelector("#deleteList");
+const shoppingListContainer = document.querySelector("#list");
+// adding items to list
 function addItemsToList() {
   if (addItemInput === "") {
     alert("You didn`t add anything.");
@@ -14,15 +16,19 @@ function addItemsToList() {
     createBtn(li);
   }
 }
-
+// adding items on pressing "Add item" button
 addItemBtn.addEventListener("click", () => {
   addItemsToList();
 });
-
+// add items on pressing Enter key
 addItemInput.addEventListener("keyup", (event) => {
   if (event.key === "Enter") {
     addItemsToList();
   }
+});
+
+newListBtn.addEventListener("click", () => {
+  shoppingListContainer.remove();
 });
 
 const listUl = document.querySelector(".list-group");
@@ -31,7 +37,7 @@ const lis = listUl.children;
 function createBtn(li) {
   // create remove button
   const remove = document.createElement("button");
-  remove.className = "fa-sharp fa-solid fa-xmark";
+  remove.className = "fa-solid fa-check";
   li.appendChild(remove);
   return li;
 }
@@ -40,3 +46,15 @@ function createBtn(li) {
 for (var i = 0; i < lis.length; i++) {
   createBtn(lis[i]);
 }
+
+shoppingListContainer.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    const button = event.target;
+    const li = button.parentNode;
+    const ul = li.parentNode;
+    if (button.className === `fa-solid fa-check`) {
+      li.style.backgroundColor = "lightgreen";
+      li.innerHTML = li.textContent + " - checked";
+    }
+  }
+});
